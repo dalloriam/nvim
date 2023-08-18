81c88f5e1931ba1f00bc134f0650b9e990f8f159
William Dussault <william@dussault.dev>
2023-08-18 10:59:27 -0400

    light/dark theme toggle

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,7 +2,13 @@ require("catppuccin").setup({
     flavour = "latte",
     background = {
         light = "latte",
-        dark = "mocha",
+        dark = "macchiato",
     },
 })
 vim.cmd.colorscheme "catppuccin-latte"
+
+if os.getenv('theme') == 'dark' then
+    vim.o.background = 'dark'
+else
+    vim.o.background = 'light'
+end
