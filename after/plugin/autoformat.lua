8fe065ef2fce42e8dfc5fd22929ab05dd765e8cc
William Dussault <william@dussault.dev>
2024-11-08 09:43:27 -0500

    Fix python formatting w/ black

--- after/plugin/autoformat.lua
+++ after/plugin/autoformat.lua
@@ -4,3 +4,9 @@ vim.cmd 'augroup fmt'
 vim.cmd 'augroup END'
 
 vim.g.rustfmt_autosave = 1
+vim.g.neoformat_enabled_python = { "black" }
+vim.g.neoformat_python_black = {
+  exe = "black",
+  args = { "-q", "--line-length=100", "-"},
+  stdin = 1,
+}
